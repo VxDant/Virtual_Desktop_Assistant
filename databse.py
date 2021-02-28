389620c631d09ad3e18927701edd8257ccb83dd4
Vedant Achtani <72167208+VxDant@users.noreply.github.com>
2021-03-01 00:27:19 +0530

    Update and rename test.py to databse.py

---
+++ databse.py
@@ -0,0 +1,3 @@
+contacts = {"mummy": "xyz@gmail.com", "dad": "zyz@gmail.com", "dinesh": "xyz@gmail.com"}
+
+to = {"mummy": "+91****************", "brother": "+91************"}
--- test.py
+++
@@ -1,3 +0,0 @@
-contacts = {"mummy": "sachtani7@gmail.com", "dad": "liluachtani510@gmail.com", "dinesh": "dparmar1999.dp@gmail.com"}
-
-to = {"mummy": "+918806449987", "brother": "+917829698935"}
